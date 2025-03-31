e1dc10d71e2009a42ded264722125dc12d8f7afd
Holger Kirsten <holgerkirsten@web.de>
2025-03-31 23:47:57 +0200

    nf-core scrnaseq incl concatenate

--- scripts/03_nf_scrnaseq.R
+++ scripts/03_nf_scrnaseq.R
@@ -1,71 +1,97 @@
+#!/usr/bin/env Rscript
 require(toolboxH)
 require(here)
+
+
 # prepare samples file
 # Create a samplesheet CSV for nf-core/scrnaseq
-library(data.table)
 
-combined_dir <- here("../fastq_combined")
-output_csv <- paste0(combined_dir, "/samplesheet.csv")
-
-# Get list of FASTQ files
-fastq_files <- list.files(path = combined_dir, pattern = "*.fastq.gz", full.names = TRUE)
-
-# Extract sample information
-samples <- data.table(
-    fastq_path = fastq_files,
-    sample = str_extract(basename(fastq_files), "^[^_]+")
+# Get all fastq files recursively
+allfiles <- dir("/net/ifs1/san_projekte/projekte/genstat/01_daten/2412_scRNA_lung_organoid_chip/",
+    recursive = TRUE,
+    pattern = "*.fastq.gz",
+    full.names = TRUE
 )
 
-# Create unique sample entries
-unique_samples <- unique(samples$sample)
-unique_samples
+# Convert to data table
+files_dt <- data.table(path = allfiles)
 
-# Create samplesheet
+# Extract sample, read, and run information using stringr
+files_dt[, basename := basename(path)]
+files_dt[, sample := str_extract(basename, "^(S[1-4])")]
+files_dt[, read := str_extract(basename, "R[12]")]
+files_dt[, run := str_extract(basename, "_[001-2]+")]
 
-samplesheet <- lapply(unique_samples, function(s) {
-    # s = unique_samples[1]
-    sample_files <- samples[sample == s, fastq_path]
+files_dt[, .N, read]
+files_dt[, .N, run]
 
-    # Identify read types (R1, R2, I1)
-    r1_file <- sample_files[grep("_R1_", sample_files)]
-    r2_file <- sample_files[grep("_R2_", sample_files)]
-    # i1_file <- sample_files[grep("_I1_", sample_files)] # already demultiplexed, i.e. no multiple libraries from different project from seq center present
+# Create samplesheet according to nf-core/scrnaseq documentation
+# Each sample will appear multiple times (for each run)
+samplesheet <- dcast.data.table(
+    files_dt,
+    sample + run ~ read,
+    value.var = "path"
+)
 
-    data.table(
-        sample = s,
-        fastq_1 = r1_file,
-        fastq_2 = r2_file
-        # ,
-        # expected_cells = 10000 # Note that since cellranger v7, it is not recommended anymore to supply the --expected-cells parameter.
-        # ,
-        # fastq_3 = i1_file
-    )
-}) %>% rbindlist()
-samplesheet
+# save
+output_csv <- here("results/03_samplesheet.csv")
+
+samplesheet_save <- samplesheet[, .(sample, fastq_1 = R1, fastq_2 = R2)]
+samplesheet_save
 # Write to CSV
-fwrite(samplesheet, output_csv)
-cat("Sample sheet created:", output_csv, "\n")
+
+fwrite(samplesheet_save, output_csv)
+cat("Samplesheet created:", output_csv, "\n")
+file.exists(output_csv)
+
+
+# references
+# https://www.10xgenomics.com/support/software/cell-ranger/downloads#reference-downloads
+# md5sum: a7b5b7ceefe10e435719edc1a8b8b2fa
+
+# Check MD5 checksum of reference file
+expected_md5 <- "a7b5b7ceefe10e435719edc1a8b8b2fa"
+file_to_check <- here("results/cellranger1/refdata-gex-GRCh38-2024-A.tar.gz") # Replace with actual file path
+
+
+actual_md5_pre <- system2("md5sum", file_to_check, stdout = TRUE)
+actual_md5 <- substr(actual_md5_pre, 1, 32)
+actual_md5 == expected_md5
 
 # Clone the repository (if you haven't already)
-setwd(here("../nf_scrnaseq"))
-system("git clone https://github.com/nf-core/scrnaseq.git")
+# setwd(here("../nf_scrnaseq"))
+# system("git clone https://github.com/nf-core/scrnaseq.git")
 
-# test the pipeline
+
+
+# teste pipeline
 
 setwd(here("../nf_scrnaseq/scrnaseq"))
-# system("nextflow run nf-core/scrnaseq -profile test,singularity --outdir minitest --validate_params=false")
-# does not run, error
-# RuntimeError: cannot cache function 'sparse_mean_var_minor_axis': no locator available for file '/usr/local/lib/python3.8/site-packages/scanpy/preprocessing/_utils.py'
+
+output_results <- here("results/cellranger1")
+output_results
+
+fasta_fn <- here("results/cellranger1/refdata-gex-GRCh38-2024-A/fasta/genome.fa")
+file.exists(fasta_fn)
+
+gtf_fn <- here("results/cellranger1/refdata-gex-GRCh38-2024-A/genes/genes.gtf")
+file.exists(gtf_fn)
 
 
-# Run the pipeline with your combined FASTQ files
-# nextflow run main.nf \
-#   --input samplesheet.csv \
-#   --genome GRCh38 \
-#   --phenotype_data <path_to_phenotype_file> \
-#   --outdir <output_directory> \
-#   --protocol 10XV3 \
-#   -profile docker
+
+call2 <- paste(
+    "nextflow run nf-core/scrnaseq --input ",
+    output_csv,
+    " --outdir ", output_results,
+    " --aligner cellranger",
+    " --protocol 10XV3",
+    " --fasta", fasta_fn,
+    " --gtf", gtf_fn,
+    " -profile singularity"
+)
+
+system(call2)
+
 
 finalizeSkript()
 
